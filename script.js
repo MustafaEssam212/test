5c441fbd7e7bcb1568207ab63d6ac8e4b0cfc938
MustafaEssam212 <zaamalekzamalek62@yahoo.com>
2021-01-11 21:51:38 +0200

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+function myToggle(){
+  document.getElementById('ulll').classList.toggle('sas');
+  document.getElementById('ulll').style.animation = '.5s anim linear forwards';
+}
+
+
+function nikeOne(){
+ 
+  document.getElementById('rii').style.backgroundColor = '#ab1201';
+}
+
+
+function nikeTwo(){
+
+  document.getElementById('rii').style.backgroundColor = '#313131';
+}
+
+
+
+function nikeThree(){
+
+  document.getElementById('rii').style.backgroundColor = '#522c3d';
+}
+
+
+function mySlide(anything){
+  document.getElementById('nike1').src = anything;
+}
